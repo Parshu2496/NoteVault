6eb971954b28145d1e8326d3a600fed8ebd2e4d9
parshu2496 <prashantnrwd@gmail.com>
2026-02-05 14:15:32 +0530

    Frontend ready

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -1,6 +1,6 @@
 var jwt = require('jsonwebtoken');
 
-const JWT_SECRET = 'prashantswebsite'; 
+const JWT_SECRET = process.env.JWT_SECRET; 
 
 const fetchuser = (req, res, next) => {
     // Get the user from the jwt token and add id to req object
